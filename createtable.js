147f279378e75aa9f74442238dcd75d585c71879
Ashwaq <36631019+ayaa11@users.noreply.github.com>
2019-04-07 20:04:47 +0100

    JSON Tutorial Website

---
+++ createtable.js
@@ -0,0 +1,56 @@
+function CreateTable() {
+   var myBooks = [
+       {
+           "Book ID": "1",
+           "Book Name": "Computer Architecture",
+           "Category": "Computers",
+           "Price": "125.60"
+       },
+       {
+           "Book ID": "2",
+           "Book Name": "Asp.Net 4 Blue Book",
+           "Category": "Programming",
+           "Price": "56.00"
+        },
+        {
+            "Book ID": "3",
+            "Book Name": "Popular Science",
+            "Category": "Science",
+            "Price": "210.40"
+        }
+   ]
+
+   // EXTRACT VALUE FOR HTML HEADER. 
+   var col = [];
+   for (var i = 0; i < myBooks.length; i++) {
+       for (var key in myBooks[i]) {
+           if (col.indexOf(key) === -1) {
+               col.push(key);
+           }
+       }
+   }
+
+   var table = document.createElement("table");
+   // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
+   var tr = table.insertRow(-1); // row
+   for (var i = 0; i < col.length; i++) {
+       var th = document.createElement("th"); // header
+       th.innerHTML = col[i];
+       tr.appendChild(th);
+   }     
+
+   // ADD JSON DATA TO THE TABLE AS ROWS.
+   for (var i = 0; i < myBooks.length; i++) {
+       tr = table.insertRow(-1);
+
+       for (var j = 0; j < col.length; j++) {
+           var tabCell = tr.insertCell(-1);
+           tabCell.innerHTML = myBooks[i][col[j]];
+       }
+   }
+
+   // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
+   var divContainer = document.getElementById("showData");
+   divContainer.innerHTML = "";
+   divContainer.appendChild(table);
+}
